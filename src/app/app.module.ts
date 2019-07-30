8d1030644d2357402f6c1b5048e0483c8d4084e5
Owen Edwards <omedwards@outlook.com>
2019-07-30 15:34:56 -0400

    displaying star wars json data into the html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FilmsComponent } from './films/films.component';
 import { DatabaseService } from './services/database.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilmsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: [ DatabaseService, HttpClient],
+  providers: [DatabaseService, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
